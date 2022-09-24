9f13529a6c0b891203c4ca19f382f01d63537d5d
Mantispraying <srajanskgupta@gmail.com>
2022-09-24 21:12:05 +0530

    Exceptions for invalid requests added

--- src/main/java/com/Mantispraying/ProjectManagementTool/services/ProjectService.java
+++ src/main/java/com/Mantispraying/ProjectManagementTool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.Mantispraying.ProjectManagementTool.services;
 
 import com.Mantispraying.ProjectManagementTool.domain.Project;
+import com.Mantispraying.ProjectManagementTool.exceptions.ProjectIdException;
 import com.Mantispraying.ProjectManagementTool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,8 +12,12 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project){
-        //Logic
-
-        return projectRepository.save(project);
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }
+        catch (Exception ex){
+            throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exists");
+        }
     }
 }
--- src/main/java/com/Mantispraying/ProjectManagementTool/web/ProjectController.java
+++ src/main/java/com/Mantispraying/ProjectManagementTool/web/ProjectController.java
@@ -1,24 +1,34 @@
 package com.Mantispraying.ProjectManagementTool.web;
 
 import com.Mantispraying.ProjectManagementTool.domain.Project;
+import com.Mantispraying.ProjectManagementTool.services.MapValidationErrorService;
 import com.Mantispraying.ProjectManagementTool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/api/project")
 public class ProjectController {
     @Autowired
-    ProjectService projectService;
-
+    private ProjectService projectService;
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+
+        ResponseEntity<?> errorMap = mapValidationErrorService.mapValidationService(result);
+        if(errorMap!=null)
+            return errorMap;
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<>(project1, HttpStatus.CREATED);
+
     }
 }
