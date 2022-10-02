11b26cc7725140c28350a0ac8ca86a02d40880d7
Mantispraying <srajanskgupta@gmail.com>
2022-10-02 17:05:58 +0530

    Handled project task not found error

--- src/main/java/com/Mantispraying/ProjectManagementTool/services/ProjectTaskService.java
+++ src/main/java/com/Mantispraying/ProjectManagementTool/services/ProjectTaskService.java
@@ -55,4 +55,25 @@ public class ProjectTaskService {
 
         return projectTaskRepository.findByProjectIdentifierOrderByPriority(id);
     }
+
+    public ProjectTask findPTByProjectSequence(String backlog_id, String pt_id){
+
+        Backlog backlog = backlogRepository.findByProjectIdentifier(backlog_id);
+        if(backlog==null){
+            throw new ProjectNotFoundException("Project with ID: '"+backlog_id.toUpperCase()+"' doesn't exist");
+        }
+
+        ProjectTask projectTask = projectTaskRepository.findByProjectSequence(pt_id);
+        if(projectTask==null){
+            throw new ProjectNotFoundException("Project Task '"+pt_id+"' not found");
+        }
+
+        if(!projectTask.getProjectIdentifier().equals(backlog_id)){
+            throw new ProjectNotFoundException("Project Task '"+pt_id+"' does not exist in project: '"+backlog_id.toUpperCase()+"'");
+        }
+
+        return projectTask;
+    }
+
+
 }
