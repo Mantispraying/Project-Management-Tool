257897a9d54ffc1bb050a176ed813209f652ea1a
Mantispraying <srajanskgupta@gmail.com>
2022-10-02 16:29:50 +0530

    Handled project not found error

---
+++ src/main/java/com/Mantispraying/ProjectManagementTool/exceptions/ProjectNotFoundExceptionResponse.java
@@ -0,0 +1,17 @@
+package com.Mantispraying.ProjectManagementTool.exceptions;
+
+public class ProjectNotFoundExceptionResponse {
+    private String projectNotFound;
+
+    public String getProjectNotFound() {
+        return projectNotFound;
+    }
+
+    public void setProjectNotFound(String projectNotFound) {
+        this.projectNotFound = projectNotFound;
+    }
+
+    public ProjectNotFoundExceptionResponse(String projectNotFound) {
+        this.projectNotFound = projectNotFound;
+    }
+}
--- src/main/java/com/Mantispraying/ProjectManagementTool/services/ProjectTaskService.java
+++ src/main/java/com/Mantispraying/ProjectManagementTool/services/ProjectTaskService.java
@@ -1,8 +1,11 @@
 package com.Mantispraying.ProjectManagementTool.services;
 
 import com.Mantispraying.ProjectManagementTool.domain.Backlog;
+import com.Mantispraying.ProjectManagementTool.domain.Project;
 import com.Mantispraying.ProjectManagementTool.domain.ProjectTask;
+import com.Mantispraying.ProjectManagementTool.exceptions.ProjectNotFoundException;
 import com.Mantispraying.ProjectManagementTool.repositories.BacklogRepository;
+import com.Mantispraying.ProjectManagementTool.repositories.ProjectRepository;
 import com.Mantispraying.ProjectManagementTool.repositories.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,28 +17,42 @@ public class ProjectTaskService {
     @Autowired
     private ProjectTaskRepository projectTaskRepository;
 
+    @Autowired
+    private ProjectRepository projectRepository;
+
     public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask){
-        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
-        projectTask.setBacklog(backlog);
+        try{
+            Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+            projectTask.setBacklog(backlog);
 
-        Integer BacklogSequence = backlog.getPTSequence();
-        BacklogSequence++;
-        backlog.setPTSequence(BacklogSequence);
-        projectTask.setProjectSequence(projectIdentifier+"-"+BacklogSequence);
-        projectTask.setProjectIdentifier(projectIdentifier);
+            Integer BacklogSequence = backlog.getPTSequence();
+            BacklogSequence++;
+            backlog.setPTSequence(BacklogSequence);
+            projectTask.setProjectSequence(projectIdentifier+"-"+BacklogSequence);
+            projectTask.setProjectIdentifier(projectIdentifier);
 
-        if(projectTask.getPriority()==null){
-            projectTask.setPriority(3);
+            if(projectTask.getPriority()==null){
+                projectTask.setPriority(3);
+            }
+
+            if(projectTask.getStatus()==""||projectTask.getStatus()==null){
+                projectTask.setStatus("TO_DO");
+            }
+
+            return projectTaskRepository.save(projectTask);
+        }catch (Exception e){
+            throw new ProjectNotFoundException("Project not found");
         }
-
-        if(projectTask.getStatus()==""||projectTask.getStatus()==null){
-            projectTask.setStatus("TO_DO");
-        }
-
-        return projectTaskRepository.save(projectTask);
     }
 
-    public Iterable<ProjectTask> findBacklogById(String backlog_id) {
-        return projectTaskRepository.findByProjectIdentifierOrderByPriority(backlog_id);
+    public Iterable<ProjectTask> findBacklogById(String id) {
+
+        Project project = projectRepository.findByProjectIdentifier(id);
+
+        if(project==null){
+            throw new ProjectNotFoundException("Project with ID: '"+id.toUpperCase()+"' doesn't exist");
+        }
+
+        return projectTaskRepository.findByProjectIdentifierOrderByPriority(id);
     }
 }
