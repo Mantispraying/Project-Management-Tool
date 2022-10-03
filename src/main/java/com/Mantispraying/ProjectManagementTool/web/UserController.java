401550d8f26c29b7c092fd28ebbaba4467176263
Mantispraying <srajanskgupta@gmail.com>
2022-10-03 21:57:54 +0530

    More Create User validations

--- src/main/java/com/Mantispraying/ProjectManagementTool/web/UserController.java
+++ src/main/java/com/Mantispraying/ProjectManagementTool/web/UserController.java
@@ -3,6 +3,7 @@ package com.Mantispraying.ProjectManagementTool.web;
 import com.Mantispraying.ProjectManagementTool.domain.User;
 import com.Mantispraying.ProjectManagementTool.services.MapValidationErrorService;
 import com.Mantispraying.ProjectManagementTool.services.UserService;
+import com.Mantispraying.ProjectManagementTool.validator.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,10 +23,13 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private UserValidator userValidator;
 
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result){
         // Validate passwords match
+        userValidator.validate(user,result);
 
         ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
         if(errorMap != null)return errorMap;
